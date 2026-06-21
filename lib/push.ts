b7e3dd8ae2fc176e81b2a5c1d24bba0a59d249b1
fbrunodev <brunoxl.1234@gmail.com>
2026-06-21 15:52:28 -0300

    fix: push subscription ArrayBuffer type error

--- lib/push.ts
+++ lib/push.ts
@@ -14,7 +14,7 @@ export async function registerPushSubscription(): Promise<void> {
 
   const subscription = await registration.pushManager.subscribe({
     userVisibleOnly: true,
-    applicationServerKey: urlBase64ToUint8Array(publicKey),
+    applicationServerKey: urlBase64ToUint8Array(publicKey) as ArrayBuffer,
   });
 
   const json = subscription.toJSON() as { endpoint: string; keys: { p256dh: string; auth: string } };
@@ -26,11 +26,11 @@ export async function registerPushSubscription(): Promise<void> {
   });
 }
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): ArrayBuffer {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
   const rawData = window.atob(base64);
   const outputArray = new Uint8Array(rawData.length);
   for (let i = 0; i < rawData.length; i++) outputArray[i] = rawData.charCodeAt(i);
-  return outputArray;
+  return outputArray.buffer as ArrayBuffer;
 }
